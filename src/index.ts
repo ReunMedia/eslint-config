09ba0f6a4ba6bf63d8be6dc12353ff1408253a49
Kimmo Salmela <kimmo.salmela@reun.eu>
2025-07-09 13:03:49 +0300

    WIP - Add base config file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+// @ts-check
+
+/**
+ * Reun Media typescript-eslint configuration
+ *
+ * @author Reun Media <company@reun.eu>
+ * @copyright 2024 Reun Media
+ *
+ * @see https://github.com/ReunMedia/project-templates
+ *
+ * @version 1.4.0
+ */
+
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+    languageOptions: {
+      globals: {
+        // ...globals.browser, // Use when targeting browser
+        // ...globals['shared-node-browser'] // Use for both Node and browser
+      },
+    },
+  },
+  includeIgnoreFile(gitignorePath)
+);
